4f3120836461bafbc664ba50e08a458951020594
tempiltinbek <140199954+tempiltinbek@users.noreply.github.com>
2023-07-22 01:59:59 +0500

    asasas
    fdfdf

---
+++ src/db/Main.js
@@ -0,0 +1,13 @@
+export const mainData = {
+    main_name:"Ziyome",
+    navbar:{
+        uz:{
+            search:"Qidirmoq...",
+            logout:"Chiqish"
+        },
+        en:{
+            search:"Search...",
+            logout:"Log Out"
+        }
+    }
+}
